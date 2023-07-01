289e3c496afa64192378c4b094ed59f272f51ecd
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2023-07-01 15:01:00 +0300

    general styled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import user from 'data/user.json';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
+import { Section } from './Section/Section.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Section>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        React homework template
+      </div>
+    </Section>
   );
 };
