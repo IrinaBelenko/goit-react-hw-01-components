189942088be87b4a3be634bced835c6308a111d7
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2023-07-01 15:21:24 +0300

    task border

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,22 +3,38 @@ import data from 'data/data.json';
 import friends from 'data/friends.json';
 import transactions from 'data/transactions.json';
 import { Section } from './Section/Section.styled';
+import { Container } from './Container/Container.styled';
+import { Heading } from './Heading/Heading.styled';
 
 export const App = () => {
   return (
     <Section>
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        React homework template
-      </div>
+      <Container>
+        <Heading marginBottom="50px" textAlign="center">
+          Task 1
+        </Heading>
+        <div
+          style={{
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: 40,
+            color: '#010101',
+          }}
+        >
+          React homework template
+        </div>
+        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
+          Task 2
+        </Heading>
+        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
+          Task 3
+        </Heading>
+        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
+          Task 4
+        </Heading>
+      </Container>
     </Section>
   );
 };
